401d8689a8fcf8ce3e3ae9207936a7b5ed2b6ed0
albina6 <49582478+albina6@users.noreply.github.com>
2022-03-28 04:57:31 +0500

    начала создавать функции

---
+++ NN1lab/functionMembership.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NN1lab
+{
+    class functionMembership//функция принадлежности,
+                            //нужно 4 подкласса
+    {
+        private string nameAlt;
+        private string kindOfFuncttion;
+
+        public functionMembership()
+        {
+
+        }
+
+    }
+}
---
+++ NN1lab/triangleFunction.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NN1lab
+{
+    class triangleFunction : functionMembership
+    {
+        private int a,b,c;
+
+        public triangleFunction(int a,int b,int c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+        public double triangeY(double x)
+        {
+            double y = 0.0;
+            if (a <= x && x <= b)
+            {
+                y = 1 - ((b - x) / (b - a));
+            }
+            else if (b <= x && x <= c)
+            {
+                y = 1 - ((x - c) / (c - b));
+            }
+            else
+                y = 0;
+            return y;
+        }
+    }
+}
